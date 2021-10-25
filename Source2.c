3b75e3b01eefb5c24b9528e57c9509047f017b94
UndefinedR <rafa.rossoginer@alum.uca.es>
2021-10-25 15:26:01 +0200

    v2.0

--- Source2.c
+++ Source2.c
@@ -2,6 +2,7 @@ int a;
 
 void empty();
 
+int der();
 int der(); //This gives an error since redeclaration is not allowed
 
 int fun (int c, int d){
@@ -9,10 +10,11 @@ int fun (int c, int d){
     int x = c + empty() + fun(d, a); // Ok
     x = c + empty() + fun(d, a) - der; // der is not called as function
     //if no return instruction is set, the parser gives a syntax error
-    int der();
     return c - empty() + der();
 }
 
+//This does not give an error since definitions of a previous declaration
+// are valid
 void empty(){
     int useless = 0;
     //void functions can be defined with no return instruction
