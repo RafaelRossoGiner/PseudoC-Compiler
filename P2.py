ce0b97292803039f0a0b3551e2baba8908c88f16
UndefinedR <rafa.rossoginer@alum.uca.es>
2021-10-25 21:22:41 +0200

    v2.1

--- P2.py
+++ P2.py
@@ -1,6 +1,17 @@
 from sly import Lexer
 from sly import Parser
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 class CLexer(Lexer):
     tokens = {EQUAL, LESSTHANEQUAL, GREATERTHANEQUAL, NOTEQUAL, LOGICAND, LOGICOR, ID, INTVALUE, FLOATVALUE,
@@ -98,10 +109,11 @@ class CParser(Parser):
         return p[0]
 
     # Functions
-    @_('type ID')
+    @_('type ID',
+       'VOID ID')
     def param(self, p):
         if p[1] in self.symbolValue:
-            raise RuntimeError('line ' + str(p.lineno) + ': ' + 'Redeclaration of variable ' + p[0] + ' is not allowed')
+            raise RuntimeError('line ' + str(p.lineno) + ': ' + 'Redeclaration of variable ' + p[1] + ' is not allowed')
         else:
             self.symbolValue[p[1]] = 0
         return p[1]
@@ -127,7 +139,7 @@ class CParser(Parser):
         if p[0] in self.functions:
             pass
         else:
-            raise RuntimeError('line ' + str(p.lineno) + ': ' + p[0] + ' is not a declared Function')
+            raise RuntimeError('line ' + str(p.lineno) + ': ' + p[0] + ' is not a Function')
         return 0
 
     @_('RETURN expr ";"')
@@ -138,28 +150,37 @@ class CParser(Parser):
        'type ID "(" typeDec ")"',
        'type ID "(" ")"')
     def functionDecl(self, p):
-        self.symbolValue[p[1]] = 0
-        self.functions[p[1]] = 0
         return p[1]
 
     @_('VOID ID  "(" params ")"',
        'VOID ID "(" typeDec ")"',
        'VOID ID "(" ")"')
     def voidFunctionDecl(self, p):
-        self.symbolValue[p[1]] = 0
-        self.functions[p[1]] = 0
         return p[1]
 
     @_('functionDecl ";"',
        'voidFunctionDecl ";"')
     def instruction(self, p):
+        if p[0] in self.functions:
+            raise RuntimeError('line ' + str(p.lineno) + ': Redeclaration of function ' + p[0] + ' is not allowed')
+        else:
+            if p[0] in self.symbolValue:
+                raise RuntimeError('line ' + str(p.lineno) + ': ' +p[0] + ' is already declared as a variable')
+            else:
+                self.functions[p[0]] = 0
         return 0
 
-    @_('functionDecl "{" sentence retInstruction "}"')
+    @_('functionDecl "{"',
+       'voidFunctionDecl "{"')
+    def functDefInit(self, p):
+        self.functions[p[0]] = 0
+        return 0
+
+    @_('functDefInit sentence retInstruction "}"')
     def instruction(self, p):
         return 0
 
-    @_('voidFunctionDecl "{" sentence "}"')
+    @_('functDefInit sentence "}"')
     def instruction(self, p):
         return 0
 
@@ -288,7 +309,7 @@ class CParser(Parser):
     # Simple error management
     def error(self, p):
         if p:
-            print('\033[91m', '\033[1m', "Syntax error at token", p.type, ", line: ", p.lineno)
+            print(bcolors.BOLD, bcolors.OKCYAN, "Syntax error at token", p.type, ", line: ", p.lineno)
             # Just discard the token and tell the parser it's okay.
             # self.errok()
         else:
@@ -309,4 +330,4 @@ if __name__ == '__main__':
     try:
         parser.parse(lexer.tokenize(text))
     except RuntimeError as e:
-        print('\033[91m', '\033[1m', e)
+        print(bcolors.BOLD, bcolors.OKCYAN, e)
--- Source2.c
+++ Source2.c
@@ -1,20 +1,42 @@
 int a;
 
+//This instruction gives an error since a is not declared as a function
+int hola = a;
+
+//This gives an error since "a" is already a variable
+//int a(int, int, int);
+
+//Empty declaration
 void empty();
 
-int der();
-int der(); //This gives an error since redeclaration is not allowed
+//Declaration with parameter types
+void test(int, int);
 
-int fun (int c, int d){
+// Gives an error since test is a function and must be called as a function
+//int z = test;
+
+// Does not give an error since test is called as a function
+int x = test(3, 4);
+
+//Declaration with parameter types and symbols
+int der(int f, int c, int y);
+
+//Re-declaration of a function is not allowed
+//int der();
+
+//Definition
+int fun (int g, int d) {
     int z = a;
-    int x = c + empty() + fun(d, a); // Ok
-    x = c + empty() + fun(d, a) - der; // der is not called as function
+    x = c + empty() + fun(d, a); // Ok
+
+    x = c + empty() + fun(d+test(x,z-2), a) - der();
     //if no return instruction is set, the parser gives a syntax error
     return c - empty() + der();
 }
 
-//This does not give an error since definitions of a previous declaration
-// are valid
+a = z;
+
+//This does not give an error since it is a definition of a previous declaration
 void empty(){
     int useless = 0;
     //void functions can be defined with no return instruction
