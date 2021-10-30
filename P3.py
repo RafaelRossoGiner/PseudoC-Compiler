beb5601ce6a957e86eba815d2d8a041ecec2df1e
UndefinedR <rafa.rossoginer@alum.uca.es>
2021-10-31 00:04:29 +0200

    v3.1

--- P3.py
+++ P3.py
@@ -1,5 +1,11 @@
 from sly import Lexer
-from sly import Parser
+
+global tabla
+global ta
+global ind
+global tokens
+global tokenlist
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -12,24 +18,30 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-class CLexer(Lexer):
+
+class BOOLLexer(Lexer):
     tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
     literals = {';', '(', ')'}
 
     # Tokens
-    BOOLVAL = r'true | TRUE | false | FALSE'
     ASSIGN = ':='
-    AND = r'and | AND'
-    OR = r'or | OR'
-    NOT = r'not | NOT'
-    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    AND = r'and|AND'
+    OR = r'or|OR'
+    NOT = r'not|NOT'
 
+    @_(r'true|TRUE|false|FALSE')
+    def BOOLVAL(self, t):
+        t.value = t.value.lower() == 'true'
+        return t
+
+    # Reserved keywords
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ID['print'] = PRINT
+
+    # Ignores
     ignore_space = r' '
     ignore_tabs = r'\t'
     ignore_comments = r'//.*'
-
-    # Reserved keywords
-    ID['print'] = PRINT
 
     # Error and Indexing management
     @_(r'\n+')
@@ -41,73 +53,167 @@ class CLexer(Lexer):
         print("Illegal character '%s'" % t.value[0])
         self.index += 1
 
-class CParser(Parser):
-    tokens = CLexer.tokens
-    start = 'sentence'
+def yyerror(msj):
+    global ta
+    print("Error sintactico", msj, "linea", ta.lineno)
 
 
-    # Program structure
-    @_('instruction sentence')
-    def sentence(self, p):
-        return
+# fin de yyerror()
 
-    @_('')
-    def sentence(self, p):
-        return
+def cuadra(obj):
+    global ta, ind, tokenlist
+    if ta.type == obj:
+        # print("cuadro ta = ", ta.value)
+        ind += 1
+        if ind < len(tokenlist):
+            ta = tokenlist[ind]
+            # print("==> nuevo ta = ", ta.value)
+    else:
+        yyerror("en cuadra");
 
-    @_('ID ASSIGN expr ;')
-    def instruction(self, p):
-        return
 
-    @_('PRINT expr ;')
-    def instruction(self, p):
-        return
+# fin de cuadra()
 
-    # Operations
-    @_('logicalAND logicalORPrima')
-    def logicalOR (self, p):
-        return
+def sentence():
+    global ta, tokens
+    # tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
+    # tokens = {AND, ASSIGN, BOOLVAL, ID, NOT, OR, PRINT}
+    if ta.type == 'PRINT' or ta.type == 'ID':
+        instruction()
+        sentence()
+    else:
+        if ind < len(tokenlist):
+            yyerror("en sentence")
 
-    @_('OR logicalAND logicalORPrima')
-    def logicalORPrima(self, p):
-        return
 
-    @_('')
-    def logicalORPrima(self, p):
-        return
+def instruction():
+    global ta, tokens, tabla
+    # tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
+    if ta.type == 'PRINT':
+        cuadra('PRINT')
+        print("Resultado es", expr())
+        cuadra(';')
+    elif ta.type == 'ID':
+        IDlexval = ta.value
+        cuadra('ID')
+        cuadra('ASSIGN')
+        tabla[IDlexval] = expr()
+        cuadra(';')
+    else:
+        yyerror("en instruction")
 
-    @_('unary logicalANDPrima')
-    def logicalAND(self, p):
-        return
+def unary():
+    global ta, tokens
+    # tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
+    # tokens = {AND, ASSIGN, BOOLVAL, ID, NOT, OR, PRINT}
+    if ta.type == 'NOT':
+        cuadra('NOT')
+        return not unary()
+    elif ta.type == 'BOOLVAL' or ta.type == 'ID' or ta.type == '(':
+        return bool()
+    else:
+        yyerror("en unary")
 
-    @_('AND unary logicalANDPrima')
-    def logicalANDPrima(self, p):
-        return
 
-    @_('')
-    def logicalANDPrima(self, p):
-        return
+def logicalANDPrima():
+    global ta, tokens
+    # tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
+    if ta.type == 'AND':
+        cuadra('AND')
+        logicVal = unary()
+        logicVal1 = logicalORPrima()
+        if logicVal1 is None:
+            return logicVal
+        else:
+            return logicVal or logicVal1
 
-    @_('NOT unary')
-    def unary(self, p):
-        return
 
-    @_('bool')
-    def unary(self, p):
-        return
+def logicalORPrima():
+    global ta, tokens
+    # tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
+    # tokens = {AND, ASSIGN, BOOLVAL, ID, NOT, OR, PRINT}
+    if ta.type == 'OR':
+        cuadra('OR')
+        logicVal = logicalAND()
+        logicVal1 = logicalORPrima()
+        if logicVal1 is None:
+            return logicVal
+        else:
+            return logicVal or logicVal1
 
-    @_('( expr )')
-    def bool(self, p):
-        return
 
-    @_('BOOLVAL')
-    def bool(self, p):
-        return
+def logicalAND():
+    global ta, tokens
+    # tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
+    if ta.type == 'NOT' or ta.type == 'ID' or ta.type == 'BOOLVAL' or ta.type == '(':
+        logicVal = unary()
+        logicVal1 = logicalANDPrima()
+        if logicVal1 is None:
+            return logicVal
+        else:
+            return logicVal and logicVal1
+    else:
+        yyerror("en logicalAND")
 
-    @_('ID')
-    def bool(self, p):
-        return
 
-    @_('logicalOR')
-    def expr(self, p):
-        return
+def logicalOR():
+    global ta, tokens
+    # tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
+    if ta.type == 'NOT' or ta.type == 'ID' or ta.type == 'BOOLVAL' or ta.type == '(':
+        logicVal = logicalAND()
+        logicVal1 = logicalORPrima()
+        if logicVal1 is None:
+            return logicVal
+        else:
+            return logicVal or logicVal1
+    else:
+        yyerror("en logicalOR")
+
+
+def expr():
+    global ta, tokens, ind
+    # tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
+    # tokens = {AND, ASSIGN, BOOLVAL, ID, NOT, OR, PRINT}
+    if ta.type == 'NOT' or ta.type == 'ID' or ta.type == 'BOOLVAL' or ta.type == '(':
+        return logicalOR()
+    else:
+        if ta.type != ';':
+            yyerror("en expr")
+
+
+def bool():
+    global ta, tokens, tabla
+    # tokens = {PRINT, BOOLVAL, ASSIGN, AND, OR, NOT, ID}
+    # tokens = {AND, ASSIGN, BOOLVAL, ID, NOT, OR, PRINT}
+    if ta.type == '(':
+        cuadra('(')
+        s = expr()
+        cuadra(')')
+        return s
+    elif ta.type == 'BOOLVAL':
+        s = ta.value
+        cuadra('BOOLVAL')
+        return s
+    elif ta.type == 'ID':
+        IDlexval = ta.value
+        cuadra('ID')
+        return tabla[IDlexval]
+    else:
+        yyerror("en bool")
+
+
+# fin de tipo()
+
+if __name__ == '__main__':
+    global ta, tokens, ind, tabla
+    ind = 0
+    tabla = {}
+    lexer = BOOLLexer()
+    tokens = BOOLLexer.tokens
+    tokens = sorted(tokens)
+    data = open("Source3.txt").read()
+    tokenlist = list(lexer.tokenize(data))
+    print(tokenlist)
+    ta = tokenlist[ind]
+    sentence()
+    ind = 0
