8fe438f7e369f534589dd741565b7d54e324ac24
UndefinedR <rafa.rossoginer@alum.uca.es>
2021-11-30 10:47:34 +0100

    V7.1

---
+++ Source8.c
@@ -0,0 +1,18 @@
+// Hola, comentario
+int a = 0;
+int nota = 10;
+
+while ( 0 ) {
+    int b;
+    b = b % -2;
+    while ( a ) {
+        if (nota){
+            printf("ESTO SE APRUEBA");
+        }else{
+            printf("ESTO NO SE APRUEBA");
+            while ( b%-a ) {
+                printf("hey");
+            }
+        }
+    }
+}
