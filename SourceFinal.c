98617bd706cd4becdaefa60954c3e8b6277dbba8
UndefinedR <rafa.rossoginer@alum.uca.es>
2022-01-12 02:37:49 +0100

    vEntrega

--- SourceFinal.c
+++ SourceFinal.c
@@ -1,9 +1,14 @@
-int a;
+int mac = 4;
+int a[4], b[2][7], c[4];
+int*** punt[3][6];
+
+*a = 2;
 
 int main(){
+    int result;
+    int a[5];
+    int jaja;
     printf("Hola");
     scanf("%d", a);
     return 0;
 }
-
-
